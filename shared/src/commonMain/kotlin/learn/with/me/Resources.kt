397eb2fc7d428091dea2986f3a2634023ca29a8f
Alex de Sousa <alexandre.louro.de.sousa@gmail.com>
2025-12-31 09:58:34 +0000

    chore: Extract string resources

--- shared/src/commonMain/kotlin/learn/with/me/Resources.kt
+++ shared/src/commonMain/kotlin/learn/with/me/Resources.kt
@@ -8,17 +8,25 @@ import learnwithme.shared.generated.resources.computer_image
 import learnwithme.shared.generated.resources.do_not_have_account
 import learnwithme.shared.generated.resources.email
 import learnwithme.shared.generated.resources.favorite_lessons
+import learnwithme.shared.generated.resources.favorites
+import learnwithme.shared.generated.resources.lessons
 import learnwithme.shared.generated.resources.login
 import learnwithme.shared.generated.resources.logo
+import learnwithme.shared.generated.resources.logout
 import learnwithme.shared.generated.resources.password
 import learnwithme.shared.generated.resources.password_error
 import learnwithme.shared.generated.resources.register
+import learnwithme.shared.generated.resources.settings
 
 object Resources {
     object String {
         val app_name = Res.string.app_name
         val register = Res.string.register
         val login = Res.string.login
+        val logout = Res.string.logout
+        val lessons = Res.string.lessons
+        val favorites = Res.string.favorites
+        val settings = Res.string.settings
         val do_not_have_account = Res.string.do_not_have_account
         val back = Res.string.back
         val logo = Res.string.logo
